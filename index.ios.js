02d3de517a13b4379e86422e623d5187b199557c
Sean King <kingsc@miamioh.edu>
2017-09-15 12:31:33 -0400

    Project Setup

---
+++ index.ios.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, AppRegistry } from 'react-native'
+
+const App = () => {
+  return (
+    <Text>Heyyyo</Text>
+  )
+}
+
+AppRegistry.registerComponent('albums', () => App)
